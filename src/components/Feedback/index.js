7562e50058a4538db9b805a8213ad428f6736b21
ahemmed <mohammedahemad02@gmail.com>
2023-05-10 04:57:31 +0000

    solution

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -4,21 +4,46 @@ import './index.css'
 import Emojie from '../emojies'
 
 class Feedback extends Component {
+  state = {changeItem: true}
+
+  onClickingHappy = () => {
+    this.setState({changeItem: false})
+  }
+
   render() {
     const {resources} = this.props
+    const {changeItem} = this.state
 
     return (
       <div className="bg-container">
-        <div className="text-container">
-          <h1 className="heading">
-            How satisfied are you with our customer support performance
-          </h1>
-          <ul>
-            {resources.map(eachCart => (
-              <Emojie eachCart={eachCart} key={eachCart.id} />
-            ))}
-          </ul>
-        </div>
+        {changeItem ? (
+          <div className="text-container">
+            <h1 className="heading">
+              How satisfied are you with our customer support performance
+            </h1>
+            <ul className="emojies-container">
+              {resources.emojis.map(eachCart => (
+                <Emojie
+                  eachCart={eachCart}
+                  onClickingHappy={this.onClickingHappy}
+                  key={eachCart.id}
+                />
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div className="happy-feedback-description">
+            <img
+              src={resources.loveEmojiUrl}
+              alt="love emoji"
+              className="hart-img"
+            />
+            <h1 className="happy-heading">Thank You!</h1>
+            <p className="review-description">
+              We will use your feedback to improve our customer care support.
+            </p>
+          </div>
+        )}
       </div>
     )
   }
--- src/components/emojies/index.js
+++ src/components/emojies/index.js
@@ -1,13 +1,25 @@
 import './index.css'
 
 const Emojie = props => {
-  const {eachCart} = props
+  const {eachCart, onClickingHappy} = props
   const {id, name, imageUrl} = eachCart
+
+  let onButton
+  if (id === 2) {
+    onButton = () => {
+      onClickingHappy(id)
+    }
+  }
 
   return (
     <li className="list-items">
-      <img src={imageUrl} alt="name" className="emoji-style" />
-      <p>{name}</p>
+      <img
+        src={imageUrl}
+        alt={name}
+        onClick={onButton}
+        className="emoji-style"
+      />
+      <p className="smaily-para">{name}</p>
     </li>
   )
 }
